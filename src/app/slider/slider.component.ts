94671063d8579f56b06f923c95e0fddf52e65864
Anna-kl <klimova_88@mail.ru>
2026-02-27 13:53:46 +0300

    ааа

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -12,7 +12,7 @@ export interface Tutorial {
   images?: String;
   text?: String;
   ico?: String;
-id?: number;
+
 }
 @Component({
   selector: 'app-slider',
@@ -37,7 +37,6 @@ export class SliderComponent {
           this.mainCategory = result;
         }
     );
-    
     this.responsiveOptions = [
       {
         breakpoint: '6000px',
@@ -93,85 +92,69 @@ export class SliderComponent {
     this.tutorials = [
       {
         name: "Красота",
-        images: '/assets/img/ico_rubric/1Beauty.svg',
-        id:788
+        images: '/assets/category/beaty.jpg',
       },
-      {
-        name: "Отдых и туризм",
-        images: '/assets/img/ico_rubric/2Travel.svg',
-        id:1058
-      },
-      {
-        name: "Эзотерика",
-        images: '/assets/img/ico_rubric/3esoteric.svg',
-        id:1015
-      },
-      {
-        name: "Обучение и курсы",
-        images: '/assets/img/ico_rubric/4Studing.svg',
-        id: 1119
-      },
+      // {
+      //   name: "Отдых и туризм",
+      //   images: '/assets/img/ico_rubric/2Travel.svg',
+      // },
+      // {
+      //   name: "Эзотерика",
+      //   images: '/assets/img/ico_rubric/3esoteric.svg',
+      // },
+      // {
+      //   name: "Обучение и курсы",
+      //   images: '/assets/img/ico_rubric/4Studing.svg',
+      // },
       {
         name: "Фото, видео, аудио",
-        images: '/assets/img/ico_rubric/5PhotoVideoAudio.svg',
-        id: 476
+        images: '/assets/category/foto_video.jpg',
       },
       {
         name: "Компьютеры и IT",
-        images: '/assets/img/ico_rubric/6IT.svg',
-        id: 1629
+        images: '/assets/category/it.jpg',
       },
-      {
-        name: "Мероприятия",
-        images: '/assets/img/ico_rubric/7Celebration.svg',
-        id: 0
-      },
+      // {
+      //   name: "Мероприятия",
+      //   images: '/assets/img/ico_rubric/7Celebration.svg',
+      // },
       {
         name: "Бытовые услуги",
-        images: '/assets/img/ico_rubric/11DomesticServices.svg',
-        id: 35
+        images: '/assets/category/cleaning.jpg',
       },
+      // {
+      //   name: "Аренда, прокат",
+      //   images: '/assets/img/ico_rubric/9Rent.svg',
+      // },
+      // {
+      //   name: "Животные",
+      //   images: '/assets/img/ico_rubric/10Pets.svg',
+      // },
       {
-        name: "Аренда, прокат",
-        images: '/assets/img/ico_rubric/9Rent.svg',
-        id: 514
+        name: "Строительство",
+        images: '/assets/category/home_service.jpg',
       },
-      {
-        name: "Животные",
-        images: '/assets/img/ico_rubric/10Pets.svg',
-        id: 749
-      },
-      {
-        name: "Строительство и ремонт",
-        images: '/assets/img/ico_rubric/14Repair.svg',
-        id: 61
-      },
-      {
-        name: "Доставка груза",
-        images: '/assets/img/ico_rubric/12Transportation.svg',
-        id: 1023
-      },
-      {
-        name: "Авто/Мото",
-        images: '/assets/img/ico_rubric/13AutoMoto.svg',
-        id: 590
-      },
-      {
-        name: "Ремонт техники",
-        images: '/assets/img/ico_rubric/8Equipment.svg',
-        id: 1404
-      },
-      {
-        name: "Здоровье",
-        images: '/assets/img/ico_rubric/15Medicine.svg',
-        id: 1552
-      },
-      {
+      // {
+      //   name: "Доставка груза",
+      //   images: '/assets/img/ico_rubric/12Transportation.svg',
+      // },
+      // {
+      //   name: "Авто/Мото",
+      //   images: '/assets/img/ico_rubric/13AutoMoto.svg',
+      // },
+      // {
+      //   name: "Ремонт техники",
+      //   images: '/assets/img/ico_rubric/8Equipment.svg',
+      // },
+      // {
+      //   name: "Здоровье",
+      //   images: '/assets/img/ico_rubric/15Medicine.svg',
+      // },
+      // {
 
-        name: "Спорт",
-        images: '/assets/img/ico_rubric/16Sport.svg',
-        id: 892
-      }
+      //   name: "Спорт",
+      //   images: '/assets/img/ico_rubric/16Sport.svg',
+      // }
 
     ];
   }
@@ -200,18 +183,15 @@ export class SliderComponent {
   }
 
   chooseCardForSearch(item: Tutorial) {
-    // let main = this.mainCategory.filter(_ => _.parentId === null);
-    // let category = this.mainCategory.find(_ => _.id === item.id);
-     this._route.navigate(['search/result'], {
-      queryParams: {
-        search: null,
-        distance: null ,
-        categoryId: item.id,
-        // сложные объекты можно сериализовать:
-        gender: null,
-         geo: null,
-         address: null,
-      }
-    });
-}
+    let category = this.mainCategory.find(_ => _.name === item.name);
+    const send = {
+      search: null,
+      distance:  null ,
+      categoryId: category ? category.id : null,
+      address: '',
+      gender: [],
+      geo:  null,
+    } as ISearchRequest;
+    this._route.navigate(['search/result'],  { state: { send } });
+  }
 }
